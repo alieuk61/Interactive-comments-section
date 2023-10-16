9d6e4990d5a5693e347cbc953c1437ca0ae9c885
codewarswarrior <alieuk61@gmail.com>
2023-10-16 17:06:30 +0100

    Another small commit, working on input section.

--- my-react-app/src/main-page.jsx
+++ my-react-app/src/main-page.jsx
@@ -1,20 +1,29 @@
 import './index.css'
+import data from './data.json'
+import { useState } from 'react'
 
 function CommentsSection(){
     return(
-        <section className="main w-full h-screen bg-slate-950 flex">
+        <section className="main w-full h-screen justify-center flex-col bg-Very-light-gray">
             
             {/* div wrapper and divs for the actual comments */}
 
             {/* div for the input */}
             
-            <section className='input w-200px h-200px'>
-                <input 
-                type="text" 
-                className=''
-                placeholder='Using tailwind for the first time'
-                />
+            <section className='input w-[600px] h-[150px] flex bg-White rounded-lg p-5'>
+                <img src={data.currentUser.image.png} alt="User-profile-picture" />
+                <textarea 
+                className="resize-none h-[100px] w-[500px] border-2 border-Grayish-blue rounded-md"
+                name="juliusomo"
+                id="juliusomo"
+                placeholder='Add a comment...'
+                // value={}
+                // onChange={}
+                onClick={(e) => {
                     
+                }}
+                ></textarea>
+                <div className='btn rounded-lg bg-Moderate-blue w-[90px] h-[47px] flex justify-center items-center text-White cursor-pointer'>SEND</div>
 
                 
             </section>
--- my-react-app/tailwind.config.js
+++ my-react-app/tailwind.config.js
@@ -4,7 +4,19 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        'Moderate-blue': '#5457B6',
+        'Soft Red': '#ED6468',
+        'Light grayish blue': '#C3C4EF',
+        'Pale red': '#FFB8BB',
+        'Dark-blue': '#324152',
+        'Grayish-Blue': '#67727E',
+        'Light-gray': '#EAECF1',
+        'Very-light-gray': '#F5F6FA',
+        'White': '#FFFFFF'
+      }
+    },
   },
   plugins: [],
 }
